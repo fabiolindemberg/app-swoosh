b389e3631298fcb2e02c46f6027cd80802c7f434
Fabio Lindemberg <fabiolindemberg@MacBook-Pro-Fabio-Lindemberg.local>
2018-03-16 00:25:41 -0300

    Add class description

--- app-swoosh/app-swoosh/Views/BorderButton.swift
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -5,6 +5,8 @@
 //  Created by Fabio Lindemberg on 16/03/2018.
 //  Copyright © 2018 Fabio Lindemberg. All rights reserved.
 //
+//  Goal:
+//  This class adds a custom border to a button
 
 import UIKit
 
